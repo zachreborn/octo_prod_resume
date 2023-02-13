0b9b812507c0436887864389df4f3f3a5a964352
Zachary Hill <zhill@thinkstack.co>
2023-02-13 18:40:01 -0500

    updated to use a dynamic resource

--- s3.tf
+++ s3.tf
@@ -4,8 +4,19 @@
 module "s3_prod_website_pub_bucket" {
     source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
     
-    policy = file("global/s3/bucket_policies/resume_website_policy.json")
+    # policy = file("global/s3/bucket_policies/resume_website_policy.json")
     bucket_prefix = "zachary-hill-resume"
+    policy = jsonencode({
+        Statement = [{
+            Sid        = "PublicReadGetObject"
+            Effect     = "Allow"
+            Action     = [
+                "s3:GetObject"
+            ]
+            Principal = "*"
+            Resource  = "${module.s3_prod_website_pub_bucket.arn}"
+            }]
+    })
 
     tags                   = {
         terraform   = "true"
