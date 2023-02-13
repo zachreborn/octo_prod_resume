cd078cc144398f862654c87289eae65001c67a22
Zachary Hill <zhill@thinkstack.co>
2023-02-13 18:17:36 -0500

    updated to use the development branch for testing

--- s3.tf
+++ s3.tf
@@ -2,12 +2,10 @@
 # S3 Modules
 ################################################################################################################################
 module "s3_prod_website_pub_bucket" {
-    source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=v1.17.2"
+    source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
     
     policy = file("global/s3/bucket_policies/resume_website_policy.json")
     bucket_prefix = "zachary-hill-resume"
-    region = "us-east-1"
-    acl    = "public-read"
 
     tags                   = {
         terraform   = "true"
