5ee9e1ab1d40d26b23aecb697ee06490543a42a2
Zachary Hill <zhill@thinkstack.co>
2023-02-13 17:33:00 -0500

    added s3 module for the hosting

--- s3.tf
+++ s3.tf
@@ -1,3 +1,18 @@
 ################################################################################################################################
 # S3 Modules
 ################################################################################################################################
+module "s3_prod_website_pub_bucket" {
+    source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=v1.17.2"
+    
+    policy = file("global/s3/bucket_policies/resume_website_policy.json")
+    bucket_prefix = "zachary-hill-resume"
+    region = "us-east-1"
+    acl    = "public-read"
+
+    tags                   = {
+        terraform   = "yes"
+        created_by  = "terraform"
+        environment = "prod"
+        role        = "website_bucket"
+    }
+}
