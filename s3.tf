e9fb8fea5bd0414a9e146f68848e612bd437a731
Zachary Hill <zhill@thinkstack.co>
2023-02-13 19:02:04 -0500

    adding a hiphen for easier legibility

--- s3.tf
+++ s3.tf
@@ -5,7 +5,7 @@ module "resume_website" {
     source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
     
     # policy = file("global/s3/bucket_policies/resume_website_policy.json")
-    bucket_prefix = "zachary-hill-resume"
+    bucket_prefix = "zachary-hill-resume-"
     policy = jsonencode({
         Statement = [{
             Sid        = "PublicReadGetObject"
