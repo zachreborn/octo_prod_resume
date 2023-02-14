3e3e008576f6d176c99e315f3481f8445d2f6e30
Zachary Hill <zhill@thinkstack.co>
2023-02-14 18:49:10 -0500

    renaming the bucket and adding a second dev bucket for testing purposes

--- s3.tf
+++ s3.tf
@@ -1,16 +1,31 @@
 ################################################################################################################################
 # S3 Modules
 ################################################################################################################################
-module "resume_website" {
+module "zacharyhill_prod_resume_website" {
     source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
     
     policy = file("global/s3/bucket_policies/resume_website_policy.json")
-    bucket = "zachary-hill-prod-resume"
+    bucket = "zacharyhill.co"
 
     tags   = {
         terraform   = "true"
         created_by  = "Zachary Hill"
         environment = "prod"
+        role        = "website_bucket"
+        project     = "resume"
+    }
+}
+
+module "zacharyhill_dev_resume_website" {
+    source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
+    
+    policy = file("global/s3/bucket_policies/resume_website_policy.json")
+    bucket = "dev.zacharyhill.co"
+
+    tags   = {
+        terraform   = "true"
+        created_by  = "Zachary Hill"
+        environment = "dev"
         role        = "website_bucket"
         project     = "resume"
     }
