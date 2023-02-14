387807d45485de63c8719adc3e2e7f9b1d8dacf9
Zachary Hill <zhill@thinkstack.co>
2023-02-13 19:09:32 -0500

    updated to a bucket name and updated the policy to match

--- s3.tf
+++ s3.tf
@@ -4,19 +4,8 @@
 module "resume_website" {
     source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
     
-    # policy = file("global/s3/bucket_policies/resume_website_policy.json")
-    bucket_prefix = "zachary-hill-resume-"
-    policy = jsonencode({
-        Statement = [{
-            Sid        = "PublicReadGetObject"
-            Effect     = "Allow"
-            Action     = [
-                "s3:GetObject"
-            ]
-            Principal = "*"
-            Resource  = "${module.resume_website.s3_bucket_arn}"
-            }]
-    })
+    policy = file("global/s3/bucket_policies/resume_website_policy.json")
+    bucket = "zachary-hill-prod-resume"
 
     tags                   = {
         terraform   = "true"
