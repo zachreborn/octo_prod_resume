8ff176e31fed3be5d067d2a08e20ea9ea154da82
Zachary Hill <zhill@thinkstack.co>
2023-02-13 19:00:48 -0500

    updated the resource name

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 ################################################################################################################################
 # S3 Modules
 ################################################################################################################################
-module "s3_prod_website_pub_bucket" {
+module "resume_website" {
     source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_2023_update"
     
     # policy = file("global/s3/bucket_policies/resume_website_policy.json")
@@ -14,7 +14,7 @@ module "s3_prod_website_pub_bucket" {
                 "s3:GetObject"
             ]
             Principal = "*"
-            Resource  = "${module.s3_prod_website_pub_bucket.s3_bucket_arn}"
+            Resource  = "${module.resume_website.s3_bucket_arn}"
             }]
     })
 
