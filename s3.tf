b84fe322e257dde61e9f083a66666e090a71d50f
Zachary Hill <zhill@thinkstack.co>
2023-03-10 18:37:10 -0500

    updated all modules to v2.1.1

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@
 # S3 Modules
 ################################################################################################################################
 module "zacharyhill_prod_resume_website" {
-  source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_validation_bugfix"
+  source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=v2.1.1"
   
   policy = file("global/s3/bucket_policies/prod_resume_website_policy.json")
   bucket = "zacharyhill.co"
@@ -17,7 +17,7 @@ module "zacharyhill_prod_resume_website" {
 }
 
 module "zacharyhill_dev_resume_website" {
-  source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=dev_s3_website_validation_bugfix"
+  source = "github.com/thinkstack-co/terraform-modules//modules/aws/s3_website?ref=v2.1.1"
   
   policy = file("global/s3/bucket_policies/dev_resume_website_policy.json")
   bucket = "dev.zacharyhill.co"
