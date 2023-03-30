c111709c9e6a7bb6ff9950d74e8d5d07f108825f
Zachary Hill <zhill@zacharyhill.co>
2023-03-30 16:35:31 -0400

    updated the org to octo-codes

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "thinkstack-co"
+    organization = "octo-codes"
 
     workspaces {
       name = "octo_prod_resume"
