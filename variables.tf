a0fa004d8f0e615b127c08270c6e7265ebe8c7a9
Zachary Hill <zhill@zacharyhill.co>
2023-04-13 19:30:00 -0400

    removed iam variables used in terraform cloud

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "access_id" {
-  type        = string
-  description = "AWS access id"
-  sensitive   = true
-}
-
-variable "secret_key" {
-  type        = string
-  description = "AWS secret key"
-  sensitive   = true
-}
-
 variable "aws_prod_region" {
   type        = string
   description = "AWS Prod Region"
